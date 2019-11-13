4d3bbaaf4526e2b361672f3b27cc9558a9b7834b
maxh65000 <booah8@gmail.com>
2019-11-12 22:27:07 -0500

    Add some more ways to deploy config

---
+++ deployconfig.bat
@@ -0,0 +1,1 @@
+scp src/main/deploy/config.properties admin@169.254.172.208:/home/lvuser/deploy/config.properties
---
+++ deployconfig.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+scp src/main/deploy/config.properties admin@169.254.172.208:/home/lvuser/deploy/config.properties
